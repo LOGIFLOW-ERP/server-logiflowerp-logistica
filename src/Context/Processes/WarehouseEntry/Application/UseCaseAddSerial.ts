d6aca4824fa4dc1541ed72f6540573904d3c4029
usuario0888 <antonio.mejia@logiflowerp.com>
2025-04-27 08:28:49 -0500

    feat: enhance validation in UseCaseAddSerial to include StockSerialDTO checks

--- src/Context/Processes/WarehouseEntry/Application/UseCaseAddSerial.ts
+++ src/Context/Processes/WarehouseEntry/Application/UseCaseAddSerial.ts
@@ -19,7 +19,7 @@ export class UseCaseAddSerial {
 
     async exec(_id: string, keyDetail: string, dto: StockSerialDTO) {
         await this.searchDocument(_id)
-        this.validateDetail(keyDetail)
+        this.validateDetail(keyDetail, dto)
         return this.repository.updateOne(
             {
                 _id,
@@ -38,13 +38,16 @@ export class UseCaseAddSerial {
         this.document = await this.repository.selectOne(pipeline)
     }
 
-    private validateDetail(keyDetail: string) {
+    private validateDetail(keyDetail: string, dto: StockSerialDTO) {
         const detail = this.document.detail.find(item => item.keyDetail === keyDetail)
         if (!detail) {
             throw new NotFoundException('Detalle no encontrado o ya eliminado.', true)
         }
         if (detail.serials.length >= detail.amount) {
             throw new ConflictException('Ya se ha alcanzado la cantidad máxima de series permitidas para este detalle.', true)
+        }
+        if (detail.serials.some(e => e.serial === dto.serial)) {
+            throw new ConflictException('El serial ya existe en el detalle.', true)
         }
     }
 
--- src/Context/Processes/WarehouseEntry/Application/UseCaseValidate.ts
+++ src/Context/Processes/WarehouseEntry/Application/UseCaseValidate.ts
@@ -1,13 +1,14 @@
 import { IWarehouseEntryMongoRepository } from '../Domain';
 import {
     AuthUserDTO,
-    EmployeeENTITY,
     ItemWorkflowOrderDTO,
+    OrderDetailENTITY,
     ProducType,
     ProductPriceDTO,
     ProductPriceENTITY,
     State,
     StateOrder,
+    StateStockSerialWarehouse,
     WarehouseEntryENTITY,
     WarehouseStockENTITY,
     WarehouseStockSerialENTITY,
@@ -29,7 +30,9 @@ export class UseCaseValidate {
     private document!: WarehouseEntryENTITY
     private transactions: ITransaction<any>[] = []
     private dataWarehouseStock!: WarehouseStockENTITY[]
+    private newDataWarehouseStock: WarehouseStockENTITY[] = []
     private dataWarehouseStockSerial!: WarehouseStockSerialENTITY[]
+    private newDataWarehouseStockSerial: WarehouseStockSerialENTITY[] = []
 
     constructor(
         @inject(WAREHOUSE_ENTRY_TYPES.RepositoryMongo) private readonly repository: IWarehouseEntryMongoRepository,
@@ -38,10 +41,12 @@ export class UseCaseValidate {
     async exec(_id: string, user: AuthUserDTO) {
         await this.searchDocument(_id)
         const dataProductPrices = await this.searchProductPrice()
-        await this.updateDocument(dataProductPrices, user)
         await this.searchWarehouseStocks()
         await this.searchWarehouseStocksSerial()
+        await this.updateDocument(dataProductPrices, user)
         await this.createTransactionDocument()
+        this.createTransactionWarehouseStock()
+        this.createTransactionWarehouseStockSerial()
         return this.repository.executeTransactionBatch(this.transactions)
     }
 
@@ -68,6 +73,7 @@ export class UseCaseValidate {
                 )
             }
 
+            //#region Price
             if (detail.item.producType === ProducType.SERIE && detail.serials.length !== detail.amount) {
                 throw new UnprocessableEntityException(
                     `La cantidad de series registradas no coincide con la cantidad solicitada para el código de ítem: ${detail.item.itemCode}`,
@@ -81,7 +87,22 @@ export class UseCaseValidate {
             }
 
             detail.price = await validateCustom(productPrice, ProductPriceDTO, UnprocessableEntityException)
+            //#endregion Price
+
+            //#region Position
             detail.position = i + 1
+            //#endregion Position
+
+            //#region Warehouse Stock
+            const warehouseStock = this.dataWarehouseStock.find(e => e.keySearch === detail.keySearch && e.keyDetail === detail.keyDetail)
+            if (!warehouseStock) {
+                const stock = await this.buildWarehouseStock(detail)
+                await this.buildWarehouseStockSerial(detail, stock)
+                continue
+            }
+            this.updateWarehouseStock(warehouseStock, detail)
+            await this.updateWarehouseStockSerial(warehouseStock, detail)
+            //#endregion Warehouse Stock
         }
         const validation = new ItemWorkflowOrderDTO()
         validation.date = new Date()
@@ -104,10 +125,11 @@ export class UseCaseValidate {
         this.transactions.push(transaction)
     }
 
+    //#region Stock almacen
     private async searchWarehouseStocks() {
         const keySearch = this.document.detail[0].keySearch
         const keysDetail = this.document.detail.map(e => e.keyDetail)
-        const pipeline = [{ $match: { keySearch, keyDetail: { $in: keysDetail }, state: State.ACTIVO } }]
+        const pipeline = [{ $match: { keySearch, keyDetail: { $in: keysDetail } } }]
         this.dataWarehouseStock = await this.repository.select<WarehouseStockENTITY>(
             pipeline,
             collections.warehouseStock
@@ -124,11 +146,97 @@ export class UseCaseValidate {
         )
     }
 
-    //#region Stock almacen
-    /**
-     * si hay la serie y esa serie está disponible o reservado da error
-     * 
-     */
+    private async buildWarehouseStock(detail: OrderDetailENTITY) {
+        const newStock = new WarehouseStockENTITY()
+        newStock._id = crypto.randomUUID()
+        newStock.incomeAmount = detail.amount
+        newStock.item = detail.item
+        newStock.keyDetail = detail.keyDetail
+        newStock.keySearch = detail.keySearch
+        newStock.lot = detail.lot
+        newStock.stockType = detail.stockType
+        newStock.store = this.document.store
+        const entity = await validateCustom(newStock, WarehouseStockENTITY, UnprocessableEntityException)
+        this.newDataWarehouseStock.push(entity)
+        return entity
+    }
+
+    private async buildWarehouseStockSerial(detail: OrderDetailENTITY, stock: WarehouseStockENTITY) {
+        if (detail.item.producType !== ProducType.SERIE) return
+        for (const serial of detail.serials) {
+            const newStock = new WarehouseStockSerialENTITY()
+            newStock._id = crypto.randomUUID()
+            newStock.brand = serial.brand
+            newStock.documentNumber = this.document.documentNumber
+            newStock.model = serial.model
+            newStock.serial = serial.serial
+            newStock.stock_id = stock._id
+            newStock.updatedate = new Date()
+            const entity = await validateCustom(newStock, WarehouseStockSerialENTITY, UnprocessableEntityException)
+            this.newDataWarehouseStockSerial.push(entity)
+        }
+    }
+
+    private updateWarehouseStock(warehouseStock: WarehouseStockENTITY, detail: OrderDetailENTITY) {
+        if (warehouseStock.state === State.INACTIVO) {
+            throw new BadRequestException(`El stock almacén ${warehouseStock.keyDetail} está inactivo. No se puede realizar la acción.`)
+        }
+        const transaction: ITransaction<WarehouseStockENTITY> = {
+            collection: collections.warehouseStock,
+            transaction: 'updateOne',
+            filter: { _id: warehouseStock._id },
+            update: {
+                $inc: { incomeAmount: detail.amount }
+            }
+        }
+        this.transactions.push(transaction)
+    }
+
+    private async updateWarehouseStockSerial(warehouseStock: WarehouseStockENTITY, detail: OrderDetailENTITY) {
+        if (detail.item.producType !== ProducType.SERIE) return
+        for (const serial of detail.serials) {
+            const stockSerial = this.dataWarehouseStockSerial.find(e => e.stock_id === warehouseStock._id && e.serial === serial.serial)
+            if (stockSerial) {
+                const states = [StateStockSerialWarehouse.DISPONIBLE, StateStockSerialWarehouse.RESERVADO]
+                if (states.includes(stockSerial.state)) {
+                    throw new ConflictException(`La serie ${serial.serial} en ${detail.item.itemCode} ya está registrada en el almacén y no se puede ingresar nuevamente.`, true)
+                }
+                const transaction: ITransaction<WarehouseStockSerialENTITY> = {
+                    collection: collections.warehouseStockSerial,
+                    transaction: 'updateOne',
+                    filter: { _id: stockSerial._id },
+                    update: {
+                        $set: { state: StateStockSerialWarehouse.DISPONIBLE }
+                    }
+                }
+                this.transactions.push(transaction)
+                continue
+            }
+            await this.buildWarehouseStockSerial(detail, warehouseStock)
+        }
+    }
+
+    private createTransactionWarehouseStock() {
+        for (const warehouseStock of this.newDataWarehouseStock) {
+            const transaction: ITransaction<WarehouseStockENTITY> = {
+                collection: collections.warehouseStock,
+                transaction: 'insertOne',
+                doc: warehouseStock
+            }
+            this.transactions.push(transaction)
+        }
+    }
+
+    private createTransactionWarehouseStockSerial() {
+        for (const warehouseStockSerial of this.newDataWarehouseStockSerial) {
+            const transaction: ITransaction<WarehouseStockSerialENTITY> = {
+                collection: collections.warehouseStockSerial,
+                transaction: 'insertOne',
+                doc: warehouseStockSerial
+            }
+            this.transactions.push(transaction)
+        }
+    }
     //#endregion
 
 }
